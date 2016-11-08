43715cfb6195155fed5eeb52f453ea53f3d10696
Jason <jasonphoon@gmail.com>
2016-11-08 16:49:59 +0800

    Configuration for DB

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'yii2basic',
+    'password' => 'qKNd9a5FYFE2Gx9e',
     'charset' => 'utf8',
 ];
